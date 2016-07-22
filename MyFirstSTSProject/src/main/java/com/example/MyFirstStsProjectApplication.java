ae9d2e2c27e876cb0a7aaca20b5c7f8a73539b15
elakkiyag <elakkiyag@Newt-Pk031.newtglobal.chn.local>
2016-07-22 13:25:47 +0530

    first commit from eclipse
    Signed-off-by: elakkiyag <elakkiyag@Newt-Pk031.newtglobal.chn.local>

---
+++ MyFirstSTSProject/src/main/java/com/example/MyFirstStsProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyFirstStsProjectApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		SpringApplication.run(MyFirstStsProjectApplication.class, args);
+	}
+}
